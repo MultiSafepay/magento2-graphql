03975c80ce2eb98dd74d0ea6ace03507e30dcff6
Daniel Civit <daniel.civit@multisafepay.com>
2023-02-01 18:06:43 +0100

    PLGMAG2V2-626: Fix integration test loading the correct branch of dependencies (#41)

--- .github/workflows/magento-integration-tests-pre-install.sh
+++ .github/workflows/magento-integration-tests-pre-install.sh
@@ -8,7 +8,7 @@ fi
 
 
 # PHP-SDK
-if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches/${CURRENT_HEAD_REF} | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
     PHP_SDK_BRANCH_NAME=${CURRENT_HEAD_REF}
 else
     PHP_SDK_BRANCH_NAME="master"
@@ -17,7 +17,7 @@ git clone -b ${PHP_SDK_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLO
 
 
 # MAGENTO 2 CORE MODULE
-if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-core/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-core/branches/${CURRENT_HEAD_REF} | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
     CORE_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
 else
     CORE_MODULE_BRANCH_NAME="master"
@@ -25,7 +25,7 @@ fi
 git clone -b ${CORE_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-core.git ./package-source/multisafepay/magento2-core/
 
 # MAGENTO 2 ADMIN MODULE
-if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml/branches/${CURRENT_HEAD_REF} | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
     ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
 else
     ADMIN_MODULE_BRANCH_NAME="master"
@@ -34,7 +34,7 @@ git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}
 
 
 # MAGENTO 2 FRONTEND MODULE
-if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-frontend/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-frontend/branches/${CURRENT_HEAD_REF} | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
     FRONTEND_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
 else
     FRONTEND_MODULE_BRANCH_NAME="master"
